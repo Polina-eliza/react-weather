ba0cde334f9a8f989b1ce52abc38fc5ea8755f10
Polina <polina.lipse@gmai.com>
2023-02-22 09:16:12 +0200

    provided valid href attribute

--- src/Main.js
+++ src/Main.js
@@ -8,12 +8,12 @@ const Main = () => (
         <div className="main__subheading">Week</div>
         <div className="button__wrapper">
           <button className="btn__degrees active" id="celsius-button">
-            <a className="cel" href="#" id="celsius-link">
+            <a className="cel" href="/" id="celsius-link">
               °C
             </a>
           </button>
           <button className="btn__degrees" id="fahrenheit-button">
-            <a className="fah" href="#" id="fahrenheit-link">
+            <a className="fah" href="/" id="fahrenheit-link">
               °F
             </a>
           </button>
