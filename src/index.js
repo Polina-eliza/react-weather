abc9b7a97bf52d1e1776bf2f524fbaaa3cdbc576
Polina <polina.lipse@gmai.com>
2023-02-22 09:41:55 +0200

    deleted webvitals and test files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import Sidebar from "./Sidebar";
 import Main from "./Main";
 import Author from "./Author";
