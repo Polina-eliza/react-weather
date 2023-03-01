a5295ec632c2e49e7373b55dcfe3da9c8b93977e
Polina <118730269+Polina-eliza@users.noreply.github.com>
2023-03-01 16:20:18 +0200

    added api to WeekForecast

--- src/WeekForecast.js
+++ src/WeekForecast.js
@@ -1,10 +1,30 @@
 import React from "react";
 import WeatherForecastDay from "./WeatherForecastDay";
+import axios from "axios";
 import "./WeekForecast.css";
 
-function WeekForecast(props) {
-  const { forecastData } = props;
+export default function WeekForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
 
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  function load() {
+  let lon = coordinates.longitude;
+  let lat = coordinates.latitude;
+  let apiKey = "6d48t90aao34607b488607a8df81d2bd";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${lon}&lat=${lat}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(handleResponse);
+  }
+
+  if (loaded) {
   return (
     <div>
       <div className="main">
@@ -33,6 +53,9 @@ function WeekForecast(props) {
   </div>
   </div>
 );
+} else {
+  load()
+  return null;
+}
 }
 
-export default WeekForecast;
